17edb424ee65bfbe51ec86d41180141a23a6dfca
roppert <roppert@gmail.com>
2017-02-26 16:07:27 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # sc2buildorderplayer
+
 StarCraftII Build Order Player
+
+*Caveat: This script is supplied as-is. I take no responsibilities what so ever and I do not supply support of any kind.*
+
+## About
+
+This a simple tool to help with practicing build orders in StarCraftII. If you're on Windows I recommend to look at [The Spawning Tool Build Advisor](http://store.overwolf.com/app/overwolf-spawning_tool_build_advisor).
+
+The script is only tested and verified to run on Ubuntu 16.10 but should run anywhere if the prerequisites are satisfied.
+
+## Prerequisites
+
+ * Python
+ * PyGTK+3
+
+Also this script is not very useful if you don't have a second screen to keep the build order player on. I haven't figured out how to create a proper overlay to stay on top of a fullscreen application. Alternatively you can run the game in windowed mode when practicing your build order.
+
+## How to use
+
+Create a text file for each build order you want to be able to use. I just copy the build order text from [Spawning Tool](http://lotv.spawningtool.com) and saves it as <build order name>.txt in the same directory as this script.
+Technically each line is parsed as:
+supply gametime description
+
+That makes it very simple to write your own build orders too.
+
+With StarCraftII running, start this script, select the build order you want to practice and start a game.
+
+The script will sync the build order with the game timer using the Client API and highlight the current step in the build order.
+
+To restart press reset-button. To practice another build order simply choose it in the dropdown.
+
+## Resources
+
+ * [battle.net forum thread about the Client API](https://us.battle.net/forums/en/sc2/topic/20748195420)
+ * [The Python GTK+ 3 Tutorial](http://python-gtk-3-tutorial.readthedocs.io/en/latest/index.html)
+ * [GNOME Platform Demos in Python](https://developer.gnome.org/gnome-devel-demos/stable/py.html.en)
+ * [GTK+ 3 Reference Manual](https://developer.gnome.org/gtk3/stable/)
---
+++ bop.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Build Order Player - BOP
+
+A simple GUI to sync a build order with in game time.
+
+"""
+import sys
+import glob
+
+import cairo
+import requests
+from requests.exceptions import ConnectionError
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+from gi.repository import Gdk
+from gi.repository import GObject
+
+
+UI_URL = "http://localhost:6119/ui"
+GAMETIME_URL = "http://localhost:6119/game/displayTime"
+
+
+class BuildOrderItem(Gtk.ListBoxRow):
+    def __init__(self, data):
+        super(Gtk.ListBoxRow, self).__init__()
+        self.data = data
+        self.add(Gtk.Label(data, xalign=0))
+
+
+class BOPMainWindow(Gtk.Window):
+    def __init__(self, app):
+        Gtk.Window.__init__(self, title="SC2 Build Order Player", application=app)
+
+        self.set_default_size(400, 200)
+        self.set_border_width(10)
+        self.set_position(Gtk.WindowPosition.CENTER)
+
+        box_outer = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(box_outer)
+
+        topbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        box_outer.pack_start(topbox, True, True, 0)
+
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+        topbox.pack_end(hbox, True, True, 0)
+
+        filecombo = Gtk.ComboBoxText.new()
+        self.selected_buildorder = None
+        for filename in glob.glob("*.txt"):
+            if not self.selected_buildorder:
+                self.selected_buildorder = filename
+            filecombo.append_text(filename)
+        filecombo.connect("changed", self.filecombo_changed)
+        topbox.pack_start(filecombo, False, True, 0)
+
+        timelabeltext = Gtk.Label("Game timer", halign=Gtk.Align.START)
+        hbox.pack_start(timelabeltext, True, True, 0)
+
+        timelabel = Gtk.Label("00:00", xalign=0)
+        hbox.pack_end(timelabel, False, True, 0)
+
+        resetbutton = Gtk.Button(label="Reset")
+        resetbutton.connect("button-press-event", self.resetbutton_clicked)
+        topbox.pack_start(resetbutton, True, True, 10)
+
+        self.timelabel = timelabel
+
+        buildorderlist = Gtk.ListBox()
+        box_outer.pack_end(buildorderlist, False, True, 0)
+        buildorderlist.show_all()
+
+        self.items = []
+        self.buildorderlist = buildorderlist
+
+        self.get_buildorderlist()
+        self.set_buildorderlist()
+
+        GObject.timeout_add(1000, self.update_timelabel,None)
+
+        self.current_index = 0
+
+    def filecombo_changed(self, widget):
+        text=widget.get_model()[widget.get_active_iter()][0]
+        self.selected_buildorder = text
+        self.resetbutton_clicked(None, None)
+
+    def resetbutton_clicked(self, widget, data):
+        self.timelabel.set_markup("--:--")
+        self.clear_buildorderlist()
+        self.get_buildorderlist()
+        self.set_buildorderlist()
+
+    def clear_buildorderlist(self):
+        for item in self.buildorderlist.get_children():
+            self.buildorderlist.remove(item)
+
+    def get_buildorderlist(self):
+        self.items = []
+        with open(self.selected_buildorder) as fd:
+            for line in fd:
+                line = line.strip()
+                supply, gametime, item = line.split(" ", 2)
+                self.items.append(line)
+        self.current_index = 0
+
+    def set_buildorderlist(self):
+        if self.current_index < 5:
+            start_offset = 0
+            end_offset = 11
+        else:
+            start_offset = self.current_index - 5
+            end_offset = self.current_index + 6
+        selected_child = None
+        for item in self.items[start_offset:end_offset]:
+            new_item = BuildOrderItem(item)
+            if item == self.items[self.current_index]:
+                selected_child = new_item
+            self.buildorderlist.add(new_item)
+        self.buildorderlist.select_row(selected_child)
+        self.buildorderlist.show_all()
+
+    def update_buildorderlist(self, mi, ss):
+        game_ts = int(mi * 60 + ss)
+        try:
+            item = self.items[self.current_index]
+        except IndexError as err:
+            return
+        str_mi, str_ss = item.split()[1].split(":")
+        build_ts = int(str_mi) * 60 + int(str_ss)
+        if game_ts >= build_ts:
+            self.clear_buildorderlist()
+            self.set_buildorderlist()
+            self.current_index = self.current_index + 1
+
+    def is_running(self):
+        try:
+            req = requests.request('GET', UI_URL)
+        except ConnectionError as e:
+            print("Is StarCraftII runing?")
+            return False
+        screens = req.json().get("activeScreens")
+        return True if len(screens) == 0 else False
+
+    def update_timelabel(self, data):
+        if (self.is_running()):
+            try:
+                req = requests.request('GET', GAMETIME_URL)
+            except ConnectionError as e:
+                print("Is StarCraftII runing?")
+                return False
+
+            dt = req.json().get("displayTime")
+            mi = dt//60
+            ss = dt%60
+            str = "{}:{}".format(int(mi),int(ss))
+            self.timelabel.set_markup(str)
+            self.update_buildorderlist(mi, ss)
+
+        return True
+
+
+class BOPApplication(Gtk.Application):
+    def __init__(self):
+        Gtk.Application.__init__(self)
+
+    def do_activate(self):
+        win = BOPMainWindow(self)
+        win.show_all()
+
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+
+
+if __name__ == '__main__':
+    app = BOPApplication()
+    exit_status = app.run(sys.argv)
+    sys.exit(exit_status)
+
+# vi: set fileencoding=utf-8 :
